dd8f119332149a4e3b02dfce79f221f585ca73e1
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-08-17 01:31:29 +0200

    Setup nix declarative elm project

--- shell.nix
+++ shell.nix
@@ -1,7 +1,11 @@
 { pkgs ? import <nixpkgs> { } }:
+let
+  elmProject = pkgs.callPackage ./elm-project-derivation.nix { };
+in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     elmPackages.elm
     elm2nix
+    elmProject
   ];
 }
---
+++ src/Main.elm
@@ -0,0 +1,7 @@
+module Main exposing (..)
+
+import Html exposing (text)
+
+-- create main method
+main = 
+        text "Hello Elm from TutorialsPoint"
